703129613dfe7d67ebb3bc4391d5603ec635d3e0
github username <github email address>
2020-02-03 14:19:59 -0500

    done

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,33 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select{|membership| membership.gym == self}
+  end
+
+  def lifters
+    self.memberships.map{|membership| membership.lifter}
+  end
+
+  def lifter_names
+    self.lifters.map{|lifter| lifter.name}
+  end
+
+  def total_lift_total
+    self.lifters.reduce(0){|total_lifted, lifter|
+      total_lifted + lifter.lift_total
+    }
   end
 end
--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,18 @@
 class Membership
-  attr_reader :cost
+  attr_reader :cost, :gym, :lifter
 
-  def initialize(cost)
+  @@all = []
+
+  def initialize(lifter, gym, cost)
+    @lifter = lifter
+    @gym = gym
     @cost = cost
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
 end

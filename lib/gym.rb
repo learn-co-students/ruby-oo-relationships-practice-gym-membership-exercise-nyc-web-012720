51d6c581f8658743ff773ce25b122083f8aa43d2
wags83 <gpwagner@gmail.com>
2020-02-03 14:31:32 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,30 @@
 class Gym
-  attr_reader :name
+
+  attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|membership| membership.gym == self}
+  end
+
+  def have_membership
+    members = []
+    self.memberships.each do |memberships|
+      members << memberships.lifter
+    end
+    members
+  end
+
+  def gym_lift_total
+    self.have_membership.reduce(0) {|sum, lifter| sum + lifter.lift_total}
   end
 end

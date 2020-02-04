97664b7020f53cf6ffbb005e9f047fc3a2ed1e4a
Babyyodabc <rjwahba@gmail.com>
2020-02-04 14:17:17 -0500

    Completed_lesson_Feb_4_BabyYoda

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,33 @@
 class Gym
+
   attr_reader :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def memberships
+    Membership.all.select {|mem| mem.gym == self}
+  end
+
+  def lifters
+    #lifter objs specific to that gym
+    self.memberships.map { |mem| mem.lifter}
+  end
+
+  def lifter_names
+    self.lifters.map {|lifter| lifter.name}
+  end
+
+  def comb_lift
+    self.lifters.map {|lifter| lifter.lift_total}.sum
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,41 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@lifters = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@lifters << self
+  end
+
+  def memberships
+    #gets memberships for specific lifter
+    Membership.all.select {|membership| membership.lifter == self}
+  end
+
+  def gyms
+    self.memberships.map {|mem| mem.gym}
+  end
+
+  def self.avg_lift_total
+    #this is an aggregator method
+    #find the sum of all lift totals and divide by total number of lifters
+    lift_sum = self.all.reduce(0) {|agg, lifter| agg + lifter.lift_total}
+    #lift_sum = @@lifters.map {|lifter| lifter.lift_total}.sum
+    lift_sum.to_f / @@lifters.length 
+    # Lifter.all == self.all == @@lifters(class variable)
+  end
+
+  def total_membership_cost
+    self.memberships.map {|mem| mem.cost}.sum
+  end
+
+  def sign_up(gym, cost)
+    Membership.new(cost, gym, self)
+  end
+
+  def self.all
+    @@lifters
   end
 end

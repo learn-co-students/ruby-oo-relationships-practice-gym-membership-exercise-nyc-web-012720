edad5763ec79985966259f3f80968dd384477961
komald4 <komald.4@gmail.com>
2020-02-04 15:46:59 -0500

    Done.

--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,19 @@
 class Membership
+  attr_reader :cost, :gym, :lifter
   attr_reader :cost
 
+  @@all = []
+
+  def initialize(cost, gym, lifter)
   def initialize(cost)
     @cost = cost
+    @gym = gym
+    @lifter = lifter
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end  
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,9 +1,20 @@
-# You don't need to require any of the files in lib or pry.
-# We've done it for you here.
 require_relative '../config/environment.rb'
 
 # test code goes here
 
+pilates = Gym.new("Pilates Studio")
+mercedes_club = Gym.new("Mercedes Club")
+pizza_world = Gym.new("Pizza World")
+crossfit = Gym.new("CrossFit")
 binding.pry
 
+Komal = Lifter.new("Komal", 1000)
+Savita = Lifter.new("Savita", 500)
+
+mem1= Membership.new(99, pilates, Komal)
+mem2= Membership.new(100, mercedes_club, Savita)
+mem3= Membership.new(25, pizza_world, Komal)
+mem4= Membership.new(999, crossfit, Savita)
+
+binding.pry
 puts "Gains!"

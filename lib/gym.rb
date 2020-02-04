f445c3c3973d03d84d025fcaaf9770ac1c99526d
sekoudosso82 <sekou.dosso82@gmail.com>
2020-02-04 13:04:50 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,41 @@
 class Gym
   attr_reader :name
-
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
   end
+  # Get a list of all gyms
+  def self.all 
+    @@all 
+  end 
+
+  # Get a list of all memberships at a specific gym
+  def memberships 
+    Membership.all.select do |membership|
+      membership.gym == self
+    end 
+  end 
+
+  # Get a list of all the lifters that have a membership to a specific gym
+  def lifters 
+    self.memberships.map do |membership|
+      membership.lifter
+    end
+  end 
+
+  # Get a list of the names of all lifters that have a membership to that gym
+  def names 
+    self.lifters.map do |lifter|
+      lifter.name 
+    end 
+  end 
+
+  # Get the combined lift total of every lifter has a membership to that gym
+  def combined_lift_total 
+  total = self.lifters.map do |lifter|
+    lifter.lift_total
+    end.sum
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,7 +3,33 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+# initiate lifters instances
+# def initialize(name, lift_total)
+    osgood = Lifter.new("Osgood", 200)
+    sekou = Lifter.new("Sekou", 100)
+    john = Lifter.new("John", 100)
+    kelly = Lifter.new("Kelly", 50)
+    april = Lifter.new("April", 80)
+    louis = Lifter.new("Louis", 100)
 
+# initiate gyms instances
+# def initialize(name)
+    bronx = Gym.new("Bronx")
+    manhattan = Gym.new("Manhattan")
+    brooklyn = Gym.new("Brooklyn")
+    queens = Gym.new("Queens")
+    staten_island = Gym.new("Staten Island")
+
+# initiate memberships instances
+# def initialize(cost, lifter, gym)
+
+    membership1 = Membership.new(100, osgood,manhattan)
+    membership2 = Membership.new(50, sekou, bronx)
+    membership3 = Membership.new(100, john, manhattan)
+    membership4 = Membership.new(100, kelly, brooklyn)
+    membership5 = Membership.new(80, april, queens)
+    membership6 = Membership.new(100, louis, staten_island)
+    
 binding.pry
 
 puts "Gains!"

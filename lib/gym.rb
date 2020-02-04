5206638af1dbbf3196ba1f1e19da76d077b2a44a
osgoodgunawan <ogunawan@ucsd.edu>
2020-02-04 01:23:44 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,40 @@
 class Gym
   attr_reader :name
 
+  @@all=[]
+
   def initialize(name)
     @name = name
+
+    @@all << self
   end
+
+  #Get a list of all gyms
+  def self.all
+    @@all
+  end
+
+  #Get a list of all memberships at a specific gym
+  def memberships
+    Membership.all.select{|membership| membership.gym==self}
+  end
+
+  #Get a list of all the lifters that have a membership to a specific gym
+  def lifters
+    self.memberships.map{|members| members.lifter}
+  
+  end
+
+  # Get a list of the names of all lifters that have a membership to that gym
+  def lifters_name
+   self.lifters.map{|members| members.name}
+  end
+
+
+  # Get the combined lift total of every lifter has a membership to that gym
+  def total_lift
+    self.lifters.reduce(0){|total,lifts| total+ lifts.lift_total}
+  end
+
+
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,53 @@
 class Lifter
   attr_reader :name, :lift_total
 
+   @@all=[]
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  #Get a list of all lifters
+  def self.all
+    @@all
+  end
+
+  #Get a list of all the memnberships that a specific lifter has
+  def memberships
+    Membership.all.select {|members| members.lifter==self}
+  end
+
+  #Get a list of all they gyms that a specific lifter has memberships to 
+  def gym
+    self.memberships.map{|lifter| lifter.gym}
+  end
+
+  #Get the average lift total of all lifters
+  def avg_total
+    @@all.reduce(0){|total,lifters| total+ lifters.lift_total}/@@all.length
+  end
+
+  #Get the total cost of a specific lifter's gym memberships
+  def total_cost
+    self.memberships.reduce(0){|sum,gym|sum+gym.cost}
+  end
+
+  #Given a gym and a membership cost, sign a specific lifter up for a new gym
+  def sign_up(gym,cost)
+    Membership.new(self,gym,cost)
+  end
+
+
+
+
+
+
+
+
+
+
+
+
 end
